c69d77a13a15637a943ceaca867a23263e7aa4ce
Marcelo Soares Souza <marcelo@riseup.net>
2019-05-30 21:37:53 -0300

    Iniciando Backend Surittec

---
+++ src/main/java/com/surittec/webservices/surittecbackend/SurittecBackendApplication.java
@@ -0,0 +1,13 @@
+package com.surittec.webservices.surittecbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurittecBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SurittecBackendApplication.class, args);
+	}
+
+}
